7331e67f83bf3e5b74744dbe7d2ac67966112902
sgoodm <sgoodman@aiddata.org>
2017-02-10 09:46:31 -0500

    Update example and add use_cases for roads/water distance rasters

--- example.py
+++ example.py
@@ -1,13 +1,9 @@
 
 
-
-
 from main import build_distance_array
-from utils import rasterize
-
+from utils import rasterize, export_raster
 
 # -----------------------------------------------------------------------------
-
 
 shp_path = "data/line_test/line_test.shp"
 out_name = "line_test"
@@ -18,41 +14,32 @@ out_name = "line_test"
 # shp_path = "data/ca_riv_15s/ca_riv_15s.shp"
 # out_name = "ca_riv_15s"
 
-# shp_path = "/home/userz/Desktop/river_shps/world_riv_15s/world_riv_15s.shp"
-# out_name = "world_riv_15s"
-
-# shp_path = "/home/userz/Desktop/shorelines/GSHHS_f_L1_lines.shp"
-# out_name = "shorelines"
-
-
 # -----------------------------------------------------------------------------
-
 
 pixel_size = 0.01
 
-rasterized_feature_output_path = "data/{0}_raster.tif".format(out_name)
-
+rasterized_features_path = "data/{0}_binary_raster.tif".format(out_name)
 
 
 rv_array, affine = rasterize(path=shp_path, pixel_size=pixel_size,
-                             output=rasterized_feature_output_path)
+                             output=rasterized_features_path)
+
+# export_raster(rv_array, affine, rasterized_features_path)
 
 
 # print rv_array
 print rv_array.shape
 print affine
 
-
-output_raster_path = "data/{0}_distance_raster.tif".format(out_name)
-
-
 # -----------------------------------------------------------------------------
+
+distance_raster_path = "data/{0}_distance_raster.tif".format(out_name)
 
 def raster_conditional(rarray):
     return (rarray == 1)
 
 build_distance_array(rv_array, affine=affine,
-                     output=output_raster_path,
+                     output=distance_raster_path,
                      conditional=raster_conditional)
 
 
