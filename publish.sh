21caa12acb37f5cb843480bb9171c1feeb938786
sgoodm <sgoodman@aiddata.org>
2017-03-09 13:15:58 -0500

    Update setup.py and add publish script

---
+++ publish.sh
@@ -0,0 +1,2 @@
+python setup.py sdist --formats=gztar,zip bdist_wheel
+twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='distancerasters',
     packages=['distancerasters'],
-    version='0.1.0a5',
+    version='0.1.1a1',
     description='Generate distance raster using arbitrary sets of spatial features',
     author='Seth Goodman',
     author_email='sgoodman@aiddata.wm.edu',
+    license="BSD",
     url='https://github.com/itpir/python-distance-rasters',
     keywords=['raster', 'distance', 'spatial'],
     classifiers=[],
