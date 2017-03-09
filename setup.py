800e54459b20c90b97267f5a5584fcfc72237ee3
sgoodm <sgoodman@aiddata.org>
2017-03-09 11:43:29 -0500

    Change package name and add other package files

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='distance-rasters',
-    packages=['distance-rasters'],
-    version='0.1.0a0',
+    name='distancerasters',
+    packages=['distancerasters'],
+    version='0.1.0a1',
     description='Generate distance raster using arbitrary sets of spatial features',
     author='Seth Goodman',
     author_email='sgoodman@aiddata.wm.edu',
     url='https://github.com/itpir/distance-rasters',
     keywords=['raster', 'distance', 'spatial'],
     classifiers=[],
+    package_dir={'': 'src'}
 )
